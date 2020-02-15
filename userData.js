2fa4c409cd8a488dfffcfb82b49ccbd2e807de26
zhujianjie510 <zhujianjie024@pingan.com.cn>
2020-02-16 04:53:13 +0800

    nodemailer

---
+++ userData.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: new Date(),
+    },
+})
+
+module.exports = User = mongoose.model('user', userSchema) 
