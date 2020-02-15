46f2f113e0384ae2f4f5cdfb9602d3d45ce332be
zhujianjie510 <zhujianjie024@pingan.com.cn>
2020-02-16 05:35:46 +0800

    make it better

--- server.js
+++ server.js
@@ -1,94 +1,107 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const nodemailer = require('nodemailer')
-const User = require('./userData')
-require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
-const app = express()
-mongoose
-    .connect('mongodb://nodemailer:test1234@ds249137.mlab.com:49137/nodemailer-api1', { useNewUrlParser: true })
-    .then(() => {
-        console.log('mongodb connted')
-    })
-    .catch(e => console.log(e))
+const User = require('./models/User');
+
+const app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
+// connnect mongodb
+mongoose
+  .connect(
+    'mongodb://nodemailer:test1234@ds249137.mlab.com:49137/nodemailer-api1',
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
 
-app.post('/saveUserInfo', (req, res) => {
-    const reqData = req.body
-    User.findOne({name: reqData.name}).then(data => {
-        if (data) {
-            res.status(400).json({
-                status: 'failed',
-                msg: 'already existed',
-            })
-        } else {
-            const newUser = new User({
-                name: reqData.name,
-                email: reqData.email,
-                password: reqData.password,
-            })
-            newUser.save().then(data => {
-                res.status(200).json({
-                    status: 'succeed',
-                    data,
-                })
-            })
-        }
-    })
-})
+app.get('/', (req, res) => {
+  res.json({ state: 'suc', msg: 'it works' });
+});
+
+app.post('/addUser', (req, res) => {
+  // console.log(req.body);
+  User.findOne({ username: req.body.username }).then(user => {
+    if (user) {
+      res.status(400).json({
+        state: 'failed',
+        msg: '该用户已存在'
+      });
+    } else {
+      const newUser = new User({
+        username: req.body.username,
+        pwd: req.body.pwd,
+        email: req.body.email
+      });
+
+      newUser
+        .save()
+        .then(user => {
+          res.status(200).json({
+            state: 'suc',
+            msg: '添加用户成功',
+            data: user
+          });
+        })
+        .catch(err => console.log(err));
+    }
+  });
+});
 
 app.post('/retrievePwd', (req, res) => {
-    User.findOne({ name: req.body.name }).then(user => {
-        if (!user) {
-            res.status(400).json({
-                state: 'failed',
-                msg: '该用户不存在',
-            })
-        } else {
-            // step 1
-            let transporter = nodemailer.createTransport({
-                service: 'qq',
-                secure: true,
-                auth: {
-                    user: '734270721@qq.com', // 309595700@qq.com
-                    pass: 'ztfqvwgnkzjqbbgj', // 123456
-                },
-            })
-
-            // step 2
-            let mailOptions = {
-                from: '734270721@qq.com',
-                to: req.body.email,
-                subject: 'STRATAKI',
-                text: 'time to sleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeep',
-            }
-
-            // step 3
-            transporter.sendMail(mailOptions).then(data => {
-                    res.status(200).json({
-                        state: 'suc',
-                        msg: `密码已发送至您的邮箱${req.body.email}`,
-                    })
-            }).catch(err => {
-                    res.status(400).json({
-                        state: 'failed',
-                        msg: err,
-                    })
-            })
+  User.findOne({ username: req.body.username }).then(user => {
+    if (!user) {
+      res.status(400).json({
+        state: 'failed',
+        msg: '该用户不存在'
+      });
+    } else {
+      // step 1
+      let transporter = nodemailer.createTransport({
+        service: 'qq',
+        secure: true,
+        auth: {
+          user: '309595700@qq.com', // 309595700@qq.com
+          pass: 'tdnkagirlywdbigc' // 123456
         }
-    })
-})
+      });
 
-const port = process.env.port || '8000'
-app.listen(port, err => {
-    if (err) {
-        throw err
+      // step 2
+      let mailOptions = {
+        from: '309595700@qq.com',
+        to: req.body.email,
+        subject: '找回密码',
+        text: `您的用户名:${user.username},密码: ${user.pwd}`
+      };
+
+      // step 3
+      transporter.sendMail(mailOptions, (err, data) => {
+        if (err) {
+          res.status(400).json({
+            state: 'failed',
+            msg: err
+          });
+        } else {
+          res.status(200).json({
+            state: 'suc',
+            msg: `密码已发送至您的邮箱${req.body.email}`
+          });
+        }
+      });
     }
-    console.log(`port ${port} started ...`)
-})
+  });
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`服务正在运行中,端口:${port}`);
+});
